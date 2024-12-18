da7d4379471d3065d15a664747cfbf6ceebc25dc
Kaan Buke <kabuke13@gmail.com>
2024-12-18 15:24:13 +0000

    update css elements

--- client/src/roots.js
+++ client/src/roots.js
@@ -12,7 +12,7 @@ const routes = [
                 element: <Home />
             },
             {
-                path: "login",
+                path: "/login",
                 element: <Login />
             }
         ]
